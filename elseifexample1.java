b220c1f01fc806bb95e8acd7f916391a732317e8
Adarsh Kumar <adarshkumar20041@gmail.com>
2026-01-08 22:18:50 +0530

    demo java program

---
+++ elseifexample1.java
@@ -0,0 +1,22 @@
+public class elseifexample1 {
+    public static void main(String[] args) {
+        
+        int marks = 20;
+        if(marks>90)
+        {
+            System.out.println("Grade A");
+        }
+        else if(marks>70)
+        {
+            System.out.println("Grade B");
+        }
+        else if(marks>50)
+        {
+            System.out.println("Grade C ");
+        }
+        else
+        {
+            System.out.println("Fail");
+        }
+    }
+}
