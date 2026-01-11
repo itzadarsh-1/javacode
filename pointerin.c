2a24f2871dabe1f441dfe67a37e76932d59f8034
Adarsh Kumar <adarshkumar20041@gmail.com>
2026-01-11 18:32:01 +0530

    Question in Pointer

---
+++ pointerin.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    int *p1, *p2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Assign addresses to pointers
+    p1 = &a;
+    p2 = &b;
+
+    // Add values using pointers
+    sum = *p1 + *p2;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
